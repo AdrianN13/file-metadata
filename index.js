9ea616b3c0dcfb47d93744ae4b41c492b71f9353
AdrianN13 <121825231+AdrianN13@users.noreply.github.com>
2024-10-01 19:41:20 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+
+var app = express();
+
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' });
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req,res) {
+  try{
+    res.json({
+      "name": req.file.originalname,
+      "type": req.file.mimetype,
+      "size": req.file.size
+    });
+
+  }catch (error){
+    res.status(500).json({error: error.message})
+  }
+})
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
